faf123e41d327595bea49fbb32086e1abac30fe4
Daw-Chih Liou <daw-chih.liou@trivago.com>
2019-06-26 14:39:38 +0200

    feat(material-ui): integrate with material ui

---
+++ src/theme/index.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import amber from '@material-ui/core/colors/amber';
+import blue from '@material-ui/core/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: amber,
+    secondary: blue,
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default theme;
